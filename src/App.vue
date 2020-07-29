a32aaa2ece5c47a902a7fab2939e9abe66126361
mistadane <samrehonline03@gmail.com>
2020-07-29 06:08:10 -0500

    First vue.js task

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <MyName name="Samir Areh"/>
+  </div>
+</template>
+
+<script>
+import MyName from './components/MyName.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyName
+  }
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  
+</style>
---
+++ src/components/MyName.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{name}}</h1>
+    </div>
+</template>
+
+
+<script>
+export default{
+    props: {
+        name: String
+        }
+}
+</script>
+
+<style scoped>
+    h1 {
+        text-align: center;
+        font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+        font-weight: 700;
+        font-size: 50px;
+        position: relative;
+        top: 200px;
+    }
+</style>
